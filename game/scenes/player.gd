0aead929ea48b5d61759cceb22612dbcf19cc08d
Alex Paque Garns <apgarns@nmsu.edu>
2024-10-21 13:35:57 -0600

    Fix movement

--- game/scenes/player.gd
+++ game/scenes/player.gd
@@ -1,30 +1,49 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
-const JUMP_VELOCITY = -400.0
 
+func _process(delta):
+	var x_direction = 0
+	var y_direction = 0
+	
+	if Input.is_action_pressed("move_right"):
+		x_direction =+1
+		$AnimatedSprite2D.animation = "RIGHT_1"
+	if Input.is_action_pressed("move_left"):
+		x_direction =-1
+		$AnimatedSprite2D.animation = "LEFT"
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += get_gravity() * delta
+		
+	if Input.is_action_pressed("move_down"):
+		y_direction =+1
+		$AnimatedSprite2D.animation = "DOWN"
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("move_up"):
+		y_direction =-1
+		$AnimatedSprite2D.animation = "UP"
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var x_direction := Input.get_axis("move_left", "move_right")
+		
+	
+	# Handle horizontal movement
 	if x_direction:
 		velocity.x = x_direction * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	var y_direction := Input.get_axis("move_up", "move_down")
+	
+	# Handle vertical movement
 	if y_direction:
 		velocity.y = y_direction * SPEED
+
 	else:
-		velocity.y = y_direction * SPEED
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+
+	# If there is no movement, stop the animation
+	if x_direction == 0 and y_direction == 0:
+		$AnimatedSprite2D.stop()
+	else:
+		$AnimatedSprite2D.play()
+
+	# Move the character
 	move_and_slide()
