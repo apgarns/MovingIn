651c332c910ebd40e0ee42b1d0c2d1892e87d52f
Alex Paque Garns <apgarns@nmsu.edu>
2024-10-21 13:28:38 -0600

    FIX MOVEMENT

---
+++ MovingIn-main/game/scenes/main_menu/control.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_ScreenResolutionChanged():
+	# Update button sizes when resolution changes
+	$Control.rect_size = Vector2(get_viewport().get_visible_rect().size.x / 2, 20)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
